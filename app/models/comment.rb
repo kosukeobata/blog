d67cfc331bd7f4a3e0041e00b2a78de496b18402
kosukeobata <kosuke.obata@bizreach.co.jp>
2019-08-20 13:04:52 +0900

    バリデーションの追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :article
+  validates :commenter, presence: true,
+                     length: { maximum: 24 }
+
+  validates :body, presence: true,
+                     length: { maximum: 1000 }
 end
