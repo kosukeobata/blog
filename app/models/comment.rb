b2956df681244447b247402e1bb83c8f19c3a20a
kosukeobata <kosuke.obata@bizreach.co.jp>
2019-08-27 13:06:47 +0900

    コメント機能の削除

--- app/models/comment.rb
+++
@@ -1,8 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :article
-  validates :commenter, presence: true,
-                     length: { maximum: 24 }
-
-  validates :body, presence: true,
-                     length: { maximum: 1000 }
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_033239) do
+ActiveRecord::Schema.define(version: 2019_08_27_040213) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
