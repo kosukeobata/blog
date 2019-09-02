dd093b7528d125727baac6ffe22146f972354fa9
kosukeobata <kosuke.obata@bizreach.co.jp>
2019-09-03 01:21:22 +0900

    検索機能の追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,11 @@ class Article < ApplicationRecord
                     length: { minimum: 5, too_short: "%{count}文字以上を入力してください", maximum: 100, too_long: "最大%{count}文字未満で入力してください" }
   validates :text, presence: true,
                     length: { maximum: 20000 }
+  def self.search(search)
+    if search
+      where(['text LIKE ? or title LIKE ?', "%#{search}%", "%#{search}%"])
+    else
+      all
+    end
+  end
 end
