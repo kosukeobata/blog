b79d0f57f87780e8162defdc221f5a7c4886737c
kosukeobata <kosuke.obata@bizreach.co.jp>
2019-09-03 23:54:34 +0900

    articleのmodelからcommentの記載を削除

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,4 @@
 class Article < ApplicationRecord
-  has_many :comments
   validates :title, presence: true,
                     length: { minimum: 5, too_short: "%{count}文字以上を入力してください", maximum: 100, too_long: "最大%{count}文字未満で入力してください" }
   validates :text, presence: true,
