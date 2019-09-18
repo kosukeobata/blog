a126bcf1b4e6c36cf792a351141cb29f488e9fc3
kosukeobata <kosuke.obata@bizreach.co.jp>
2019-09-18 13:18:18 +0900

    herokuドメインからのリダイレクト、エラー対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class ApplicationController < ActionController::Base
    return unless /\.herokuapp.com/ =~ request.host
    
    # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める 
-   port = ":#{request.port}" unless [80, 443].include?(request.port)
-   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+#   port = ":#{request.port}" unless [80, 443].include?(request.port)
+#   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
   end
   
  end
